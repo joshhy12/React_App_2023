b5fc3ae25c0240782ace11c5d55d2ea03364028e
joshhy12 <loadywiseman@gmail.com>
2023-06-26 18:14:33 +0300

    2rd commit
    i change the navigation and so long 😎😇

--- my-app/src/pages/Layout.js
+++ my-app/src/pages/Layout.js
@@ -1,25 +1,133 @@
-import { Outlet, Link } from "react-router-dom";
+import React, { useEffect } from 'react';
+import logo from './logo.png';
+import './style.css';
 
-const Layout = () => {
+
+function Layout() {
+  useEffect(() => {
+    const body = document.querySelector('body');
+    const sidebar = body.querySelector('nav');
+    const toggle = body.querySelector('.toggle');
+    const searchBtn = body.querySelector('.search-box');
+    const modeSwitch = body.querySelector('.toggle-switch');
+    const modeText = body.querySelector('.mode-text');
+
+    toggle.addEventListener('click', () => {
+      sidebar.classList.toggle('close');
+    });
+
+    searchBtn.addEventListener('click', () => {
+      sidebar.classList.remove('close');
+    });
+
+    modeSwitch.addEventListener('click', () => {
+      body.classList.toggle('dark');
+
+      if (body.classList.contains('dark')) {
+        modeText.innerText = 'Light mode';
+      } else {
+        modeText.innerText = 'Dark mode';
+      }
+    });
+  }, []);
+
   return (
-    <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/blogs">Blogs</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
-      </nav>
+    <div>
+      {     <nav className="sidebar close">
+        <header>
+          <div className="image-text">
+            <span className="image">
+              <img src={logo} alt="" />
+            </span>
 
-      <Outlet />
-    </>
-  )
-};
+            <div className="text logo-text">
+              <span className="name">Codinglab</span>
+              <span className="profession">Web developer</span>
+            </div>
+          </div>
+
+          <i className="bx bx-chevron-right toggle"></i>
+        </header>
+
+        <div className="menu-bar">
+          <div className="menu">
+            <li className="search-box">
+              <i className="bx bx-search icon"></i>
+              <input type="text" placeholder="Search..." />
+            </li>
+
+            <ul className="menu-links">
+              <li className="nav-link">
+                <a href="#">
+                  <i className="bx bx-home-alt icon"></i>
+                  <span className="text nav-text">Dashboard</span>
+                </a>
+              </li>
+
+              <li className="nav-link">
+                <a href="#">
+                  <i className="bx bx-bar-chart-alt-2 icon"></i>
+                  <span className="text nav-text">Revenue</span>
+                </a>
+              </li>
+
+              <li className="nav-link">
+                <a href="#">
+                  <i className="bx bx-bell icon"></i>
+                  <span className="text nav-text">Notifications</span>
+                </a>
+              </li>
+
+              <li className="nav-link">
+                <a href="#">
+                  <i className="bx bx-pie-chart-alt icon"></i>
+                  <span className="text nav-text">Analytics</span>
+                </a>
+              </li>
+
+              <li className="nav-link">
+                <a href="#">
+                  <i className="bx bx-heart icon"></i>
+                  <span className="text nav-text">Likes</span>
+                </a>
+              </li>
+
+              <li className="nav-link">
+                <a href="#">
+                  <i className="bx bx-wallet icon"></i>
+                  <span className="text nav-text">Wallets</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="bottom-content">
+            <li className="">
+              <a href="#">
+                <i className="bx bx-log-out icon"></i>
+                <span className="text nav-text">Logout</span>
+              </a>
+            </li>
+
+            <li className="mode">
+              <div className="sun-moon">
+                <i className="bx bx-moon icon moon"></i>
+                <i className="bx bx-sun icon sun"></i>
+              </div>
+              <span className="mode-text text">Dark mode</span>
+
+              <div className="toggle-switch">
+                <span className="switch"></span>
+              </div>
+            </li>
+          </div>
+        </div>
+      </nav>     }
+      <section className="home">
+        <div className="text">Dashboard Sidebar and also white and dark mode</div>
+      </section>
+    </div>
+  );
+}
 
 export default Layout;
